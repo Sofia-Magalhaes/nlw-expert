346b66048bc7a2abb7091b703d391a76dd136d05
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2024-02-07 16:31:17 -0300

    build: pacote lucide-react

--- nlw-expert-notes/src/components/note-card.tsx
+++ nlw-expert-notes/src/components/note-card.tsx
@@ -1,6 +1,7 @@
-import * as Dialog from "@radix-ui/react-dialog"
-import { formatDistanceToNow } from 'date-fns'
-import { ptBR } from 'date-fns/locale'
+import * as Dialog from "@radix-ui/react-dialog";
+import { formatDistanceToNow } from "date-fns";
+import { ptBR } from "date-fns/locale";
+import { X } from 'lucide-react'
 
 interface NoteCardProps {
   note: {
@@ -23,14 +24,28 @@ export function NoteCard({ note }: NoteCardProps) {
 
       <Dialog.Portal>
         <Dialog.Overlay className="inset-0 fixed bg-black/50" />
-        <Dialog.Content className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[68vh] bg-slate-700 rounded-md flex flex-col outline-none">
+        <Dialog.Content className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[68vh] bg-slate-700 rounded-md flex flex-col outline-none">
+          <Dialog.Close className="">
+            <X/>
+          </Dialog.Close>
+
           <div className="flex flex-1 flex-col gap-3 p-5 ">
             <span className="text-sm font-medium text-slate-300">
-              {formatDistanceToNow(note.data, {locale:ptBR, addSuffix: true})}
+              {formatDistanceToNow(note.data, {
+                locale: ptBR,
+                addSuffix: true,
+              })}
             </span>
 
             <p className="text-sm leading-6 text-slate-400">{note.content}</p>
           </div>
+
+          <button 
+            type="button"
+            className="w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group"
+          >
+            Deseja  <span className="text-red-400 group-hover:underline">apagar essa nota</span>?
+          </button>
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>

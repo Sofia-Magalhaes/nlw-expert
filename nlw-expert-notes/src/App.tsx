348bd09dbf2fe3ff130b9f14897829877a556a7c
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2024-02-09 00:21:34 -0300

    feat: salvando notas
    adicionando função para salvar as notas entre componentes, comunicação de componente

--- nlw-expert-notes/src/App.tsx
+++ nlw-expert-notes/src/App.tsx
@@ -1,8 +1,24 @@
+import { useState } from "react";
 import logo from "./assets/logo-nlw-expert.svg.svg";
 import { NewNoteCard } from "./components/new-note-card";
 import { NoteCard } from "./components/note-card";
 
 export function App() {
+  const [notes, setNotes] = useState([
+    {id: 1, date: new Date(),content: "Oi gente, bão?"},
+    {id: 2, date: new Date(),content: "Oi gente, bão??"},
+  ])
+
+  function onNoteCreated(content: string){
+    const newNote = {
+      id: Math.random(),
+      date: new Date(),
+      content,
+    }
+
+    setNotes([newNote, ...notes])
+  }
+
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
       <img src={logo} alt="NLW Expert" />
@@ -18,15 +34,11 @@ export function App() {
       <div className="h-px bg-slate-700" />
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
-        <NewNoteCard />
+        <NewNoteCard onNoteCreated={onNoteCreated}/>
 
-        <NoteCard
-          note={{
-            data: new Date(),
-            content: "Oi gente, bão?",
-          }}
-        />
-        
+      {notes.map(note =>{
+        return <NoteCard key={note.id} note={note}/>
+      })}
       </div>
     </div>
   );
--- nlw-expert-notes/src/components/note-card.tsx
+++ nlw-expert-notes/src/components/note-card.tsx
@@ -5,7 +5,7 @@ import { X } from 'lucide-react'
 
 interface NoteCardProps {
   note: {
-    data: Date;
+    date: Date;
     content: string;
   };
 }
@@ -15,7 +15,10 @@ export function NoteCard({ note }: NoteCardProps) {
     <Dialog.Root>
       <Dialog.Trigger className="rounded-md text-left flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
         <span className="text-sm font-medium text-slate-300">
-          {note.data.toISOString()}
+          {formatDistanceToNow(note.date, {
+            locale: ptBR,
+            addSuffix: true,
+          })}
         </span>
         <p className="text-sm leading-6 text-slate-400">{note.content}</p>
 
@@ -26,12 +29,12 @@ export function NoteCard({ note }: NoteCardProps) {
         <Dialog.Overlay className="inset-0 fixed bg-black/50" />
         <Dialog.Content className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[68vh] bg-slate-700 rounded-md flex flex-col outline-none">
           <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
-            <X className="size-5"/>
+            <X className="size-5" />
           </Dialog.Close>
 
           <div className="flex flex-1 flex-col gap-3 p-5 ">
             <span className="text-sm font-medium text-slate-300">
-              {formatDistanceToNow(note.data, {
+              {formatDistanceToNow(note.date, {
                 locale: ptBR,
                 addSuffix: true,
               })}
@@ -40,7 +43,7 @@ export function NoteCard({ note }: NoteCardProps) {
             <p className="text-sm leading-6 text-slate-400">{note.content}</p>
           </div>
 
-          <button 
+          <button
             type="button"
             className="w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group"
           >
